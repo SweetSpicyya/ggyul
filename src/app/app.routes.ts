1012c2e14ec3982229dc550ae95ea9fda60a1c09
Yuna <urimy01@gmail.com>
2026-03-26 16:38:25 -0700

    Add new-product component and modify app.html file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { NewProduct } from './new-product/new-product';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'newproduct', component: NewProduct}
+];
---
+++ src/app/new-product/new-product.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-product',
+  imports: [],
+  templateUrl: './new-product.html',
+  styleUrl: './new-product.css',
+  standalone: true
+})
+export class NewProduct {}
